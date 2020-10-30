148b8386bac8647c76e246bf5b35d7470d0c1696
antomurugan2 <63279320+antomurugan2@users.noreply.github.com>
2020-10-30 13:58:02 -0700

    Update proc.c

--- proc.c
+++ proc.c
@@ -318,6 +318,55 @@ wait(int *status)
   }
 }
 
+// The waitpid system call
+int
+waitpid(int pid, int *status, int options)
+{
+  struct proc *p;
+  int havekids, pid1;
+  struct proc *curproc = myproc();
+
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+    // Only if pid matches
+      if (pid == p->pid) {
+        if(p->parent != curproc)
+          continue;
+        havekids = 1;
+        if(p->state == ZOMBIE){
+          // Found one.
+          // Save status
+          *status = p->status;
+          pid1 = p->pid;
+          kfree(p->kstack);
+          p->kstack = 0;
+          freevm(p->pgdir);
+          p->pid = 0;
+          p->parent = 0;
+          p->name[0] = 0;
+          p->killed = 0;
+          p->state = UNUSED;
+          release(&ptable.lock);
+          return pid1;
+        }
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
+
+}
+
 //PAGEBREAK: 42
 // Per-CPU process scheduler.
 // Each CPU calls scheduler() after setting itself up.
