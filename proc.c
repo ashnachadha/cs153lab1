76600f88b0103c8d3469aa6a953aaa2b66b03247
Ashna Chadha <achad006@sledge.cs.ucr.edu>
2020-10-30 13:12:04 -0700

    add all files

--- proc.c
+++ proc.c
@@ -225,14 +225,17 @@ fork(void)
 // An exited process remains in the zombie state
 // until its parent calls wait() to find out it exited.
 void
-exit(void)
+exit(int status)
 {
   struct proc *curproc = myproc();
   struct proc *p;
   int fd;
+  //curproc->status = status;
 
   if(curproc == initproc)
     panic("init exiting");
+
+  curproc->status = status;
 
   // Close all open files.
   for(fd = 0; fd < NOFILE; fd++){
@@ -270,7 +273,7 @@ exit(void)
 // Wait for a child process to exit and return its pid.
 // Return -1 if this process has no children.
 int
-wait(void)
+wait(int *status)
 {
   struct proc *p;
   int havekids, pid;
@@ -286,7 +289,11 @@ wait(void)
       havekids = 1;
       if(p->state == ZOMBIE){
         // Found one.
+        if(status) {
+		*status = p->status;
+	}
         pid = p->pid;
+	//curproc->status = *status;
         kfree(p->kstack);
         p->kstack = 0;
         freevm(p->pgdir);
